efab69c4bf2d47b66c61c39779b7e0c339a8ac20
oybekjon94 <x.oybek94@gmail.com>
2022-10-15 20:52:42 +0900

    [feat::] Add 1/3 data types

---
+++ src/main/kotlin/section_1/1.3.kt
@@ -0,0 +1,18 @@
+package section_1
+fun main(){
+    val whole: Int = 11
+    val fractional: Double = 11.1
+    val trueOrFalse: Boolean = true
+    val words: String = "oybek"
+    val character: Char = 'o'
+    val lines: String = """Triple quotes let 
+        you have many lines
+        in your string
+    """
+    println(whole)
+    println(fractional)
+    println(trueOrFalse)
+    println(words)
+    println(character)
+    println(lines)
+}
