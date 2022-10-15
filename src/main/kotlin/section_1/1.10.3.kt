65f4184d41b85308b57a073ef9f9938d4b7dd640
oybekjon94 <x.oybek94@gmail.com>
2022-10-16 00:07:06 +0900

    [feat::] Add 1/10/3 looping & Ranges

---
+++ src/main/kotlin/section_1/1.10.3.kt
@@ -0,0 +1,18 @@
+package section_1
+//Looping & Ranges
+fun showRange(r: IntProgression){
+    for(i in r){
+        print("$i")
+    }
+    print("    //$r")
+    println()
+}
+fun main(){
+    showRange(1..5)
+    showRange(0 until 5)
+    showRange(5 downTo 1)
+    showRange(0..9 step 2)
+    showRange(0 until 10 step 3)
+    showRange(9 downTo 2 step 3)
+
+}
