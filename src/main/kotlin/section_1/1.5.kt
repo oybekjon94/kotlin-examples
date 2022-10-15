61735246d829b7ba4f646b95f6107e599977b03b
oybekjon94 <x.oybek94@gmail.com>
2022-10-15 21:05:29 +0900

    [feat::] Add 1/5 if Expressions

---
+++ src/main/kotlin/section_1/1.5.kt
@@ -0,0 +1,10 @@
+package section_1
+// An if expression makes a choice
+fun main() {
+    if (1 > 0)
+        println("it's true")
+    if (10 < 11) {
+        println("10<11")
+        println("ten is less than eleven")
+    }
+}
