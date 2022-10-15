279b8a518c1b5176ae70103ee5e3633121ade3b9
oybekjon94 <x.oybek94@gmail.com>
2022-10-15 23:57:28 +0900

    [feat::] Add 1/10 looping & Ranges

---
+++ src/main/kotlin/section_1/1.10.kt
@@ -0,0 +1,8 @@
+package section_1
+//Looping & Ranges
+//The for keyword executes a block of code for each value in a sequence
+fun main(){
+    for (i in 1..3){
+        println("Hey $i!")
+    }
+}
