c2b9141c999b68174f6e8e1748acb99d1ae8116b
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 10:23:32 +0900

    [feat::] Add 2/2 classes

---
+++ src/main/kotlin/section_2/2.2.kt
@@ -0,0 +1,20 @@
+package section_2
+
+//creating classes
+class Giraffe
+class Bear
+class Hippo
+
+fun main(){
+    //create some  objects
+    val g1 = Giraffe()
+    val g2 = Giraffe()
+    val b = Bear()
+    val h = Hippo()
+
+    //each object() is unique:
+    println(g1)
+    println(g2)
+    println(h)
+    println(b)
+}
