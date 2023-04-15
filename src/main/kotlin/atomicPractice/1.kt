b35517db64d1e01c52d48a7414c80c17bf1f8cf4
oybekjon94 <x.oybek94@gmail.com>
2023-04-15 18:00:20 +0900

    [feat::] Add practice

---
+++ src/main/kotlin/atomicPractice/1.kt
@@ -0,0 +1,11 @@
+package atomicPractice
+
+fun main(){
+    //varAndVal
+    var whole = 11
+    var fraction = 1.4
+    var words = "oybek"
+    println(whole)
+    println(fraction)
+    println(words)
+}
