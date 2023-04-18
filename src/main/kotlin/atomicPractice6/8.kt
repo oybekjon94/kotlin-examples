1386c7a516150e8670c5cf599b7760693ae261f0
oybekjon94 <x.oybek94@gmail.com>
2023-04-18 20:51:21 +0900

    [feat::] Add practice

---
+++ src/main/kotlin/atomicPractice6/8.kt
@@ -0,0 +1,14 @@
+package atomicPractice6
+
+fun main() {
+    val myNum = 5               // Int
+    val myDoubleNum = 5.99      // Double
+    val myLetter = 'D'          // Char
+    val myBoolean = true        // Boolean
+    val myText = "Hello"        // String
+    println(myNum)
+    println(myDoubleNum)
+    println(myLetter)
+    println(myBoolean)
+    println(myText)
+}
