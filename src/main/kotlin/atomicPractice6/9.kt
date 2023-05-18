85ad4cc8123041e0785c43b6cc5812a03e74c5c5
oybekjon94 <x.oybek94@gmail.com>
2023-05-18 14:21:12 +0900

    [feat::] Add practice

--- src/main/kotlin/atomicPractice6/9.kt
+++ src/main/kotlin/atomicPractice6/9.kt
@@ -1,7 +1,14 @@
 package atomicPractice6
 
 fun main(){
-    print("what is your name?: ")
-    val name = readLine()
-    println(name)
+    val quantity = 3
+
+    val pricePerBook = when (quantity) {
+        1 -> 19.99
+        2 -> 19.99
+        3 -> 19.99
+        4 -> 19.99
+        else -> 19.99
+    }
+    println(pricePerBook)
 }
