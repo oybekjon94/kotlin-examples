0082c80dcb11c29a3a16333649bd1586404ad42e
oybekjon94 <x.oybek94@gmail.com>
2023-05-18 14:26:35 +0900

    [feat::] Add practice

--- src/main/kotlin/atomicPractice6/10.kt
+++ src/main/kotlin/atomicPractice6/10.kt
@@ -1,2 +1,11 @@
 import java.util.*
+//you can be exhaustive even without else condition
+fun main(){
+    val islightbulbOn = true
 
+    val reaction = when(islightbulbOn){
+        true -> "it's bright"
+        false -> "I can't see"
+    }
+    println(reaction)
+}
--- src/main/kotlin/atomicPractice6/9.kt
+++ src/main/kotlin/atomicPractice6/9.kt
@@ -6,8 +6,7 @@ fun main(){
     val pricePerBook = when (quantity) {
         1 -> 19.99
         2 -> 19.99
-        3 -> 19.99
-        4 -> 19.99
+        3,4 -> 19.99
         else -> 19.99
     }
     println(pricePerBook)
